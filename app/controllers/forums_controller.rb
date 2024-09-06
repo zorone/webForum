2658068aeb6d9bcd6d4b54e9b9e976fa3b83bcd9
Imron jehleh <imron.jehleh@gmail.com>
2024-09-06 20:32:15 +0700

    Okay, implement new forums path.

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,4 +1,22 @@
 class ForumsController < ApplicationController
   def index
   end
+
+  def new
+    @forum = Forum.new
+  end
+
+  def create
+    @forum = Forum.new(forum_params)
+    if @forum.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  private
+    def forum_params
+      params.require(:forum).permit(:thread, :content)
+    end
 end
