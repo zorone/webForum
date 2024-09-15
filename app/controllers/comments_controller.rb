f8352998febde97c701c45d1391ffdff410280fb
Imron jehleh <imron.jehleh@gmail.com>
2024-09-16 04:03:49 +0700

    What...

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+before_action :find_forum
+before_action :find_comment, only [:edit, :update, :destroy]
+
   def create
     @forum = Forum.find(params[:forum_id])
     @comment = @forum.comments.create(comment_params)
@@ -11,8 +14,32 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to forum_path(@forum)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to forum_path(@forum)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:message)
+  end
+
+  def find_forum
+    @forum = Forum.find(params[:forum_id])
+  end
+
+  def find_comment
+    @comment = @forum.comments.find(params[:id])
   end
 end
