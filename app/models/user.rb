19877c5da67738a32968b60bbe4174f83c8378c1
Imron jehleh <imron.jehleh@gmail.com>
2024-09-20 18:40:53 +0700

    Let's finish the work first. We can try them later.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presense: true
+  validates :email, presence: true
   validates :email, uniqueness: true
 end
--- test/models/forum_test.rb
+++ test/models/forum_test.rb
@@ -1,5 +1,6 @@
 require "test_helper"
 
+# Let's finish the work first.
 class ForumTest < ActiveSupport::TestCase
   test "forum_need_owner" do
     test_forum = Forum.where(user: nil)
