143b787f6802a7276b1cdb871ec0a204d50da1f3
Imron jehleh <imron.jehleh@gmail.com>
2024-10-03 05:28:29 +0700

    Try to implement models testing. Still won't work. I might need to focus on devise testing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+
   has_many :forums
   has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :email, presence: true
-  validates :email, uniqueness: true
 end
