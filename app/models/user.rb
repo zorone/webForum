7980d85bb3cd6ea853b30ce389e62fe8c43e9c3c
Imron jehleh <imron.jehleh@gmail.com>
2024-10-03 13:04:36 +0700

    Still won't work!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
 
   has_many :forums
   has_many :comments
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user has email" do
+      user = User.new()
+      assert user.valid?
+  end
 end
