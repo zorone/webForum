955300c98a84ddd28ea31e4b31b95dd6f54c9fda
Imron jehleh <imron.jehleh@gmail.com>
2024-10-03 12:54:40 +0700

    Great, that what we want.

--- test/models/forum_test.rb
+++ test/models/forum_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 # Let's finish the work first.
 class ForumTest < ActiveSupport::TestCase
-  include Devise::Test::ControllerHelper
+  include Devise::Test::ControllerHelpers
   test "forum_need_owner" do
     assert_nil Forum.where(user: nil)
   end
