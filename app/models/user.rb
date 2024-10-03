be282a7f3c076b9238625b672d7e24ab55637c85
Imron jehleh <imron.jehleh@gmail.com>
2024-10-03 14:06:37 +0700

    Ummm... According to tutorial, they'll prefer factory over fixture. Sure!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
   has_many :forums
   has_many :comments
