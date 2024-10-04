717e93f34ca8566b86edddb818ac010eae683077
Imron jehleh <imron.jehleh@gmail.com>
2024-10-04 21:36:48 +0700

    It still has the same issue...

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
     factory :user do
         email { Faker::Internet.email }
-        password { Faker::Internet.password(min_length: 6, max_length: 128) }
+        password { Faker::Internet.password }
     end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,10 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   test "user has email and password" do
-      user = User.new(email: "user@example.com", password: "testPassword")
+    user = build(:user)
+    attrs = attributes_for(user)
+
+      user = User.new(attrs)
       assert user.valid?
   end
 end
