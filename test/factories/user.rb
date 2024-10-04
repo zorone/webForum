3d5d3f5d25184e3f0809bf28b7dba5dac96ce127
Imron jehleh <imron.jehleh@gmail.com>
2024-10-04 21:17:07 +0700

    That's fishy, why it won't work...
    Error:
    UserTest#test_user_has_email_and_password:
    KeyError: Factory not registered: #<User id: 1, email: [FILTERED], created_at: "2024-10-04 14:16...

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
     factory :user do
         email { Faker::Internet.email }
-        password { Faker::Internet.password }
+        password { Faker::Internet.password(min_length: 6, max_length: 128) }
     end
 end
