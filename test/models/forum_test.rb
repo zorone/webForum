ac1a765648e662451a24eb4fd63068a59630142c
Imron jehleh <imron.jehleh@gmail.com>
2024-10-04 20:45:23 +0700

    Why it won't work???

--- test/models/forum_test.rb
+++ test/models/forum_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 # Let's finish the work first.
 class ForumTest < ActiveSupport::TestCase
   test "forum_need_owner" do
-    assert_nil Forum.where(user: nil)
+    result = Forum.where(user: nil)
+    assert_nil result
   end
 end
