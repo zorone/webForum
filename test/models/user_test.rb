c6541568c56814dcbdead6469e392cae10321f86
Imron jehleh <imron.jehleh@gmail.com>
2024-10-04 20:53:05 +0700

    Great! More progress.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,10 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   test "user has email" do
-      user = User.new(:user)
+      user = create(:user)
+      attrs = attributes_for(user)
+
+      user = User.new(attrs)
       assert user.valid?
   end
 end
