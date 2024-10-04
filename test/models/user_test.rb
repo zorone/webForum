c9aae5c2aa7ec2bb05590aad7edbf7e7b45a1dea
Imron jehleh <imron.jehleh@gmail.com>
2024-10-04 20:51:22 +0700

    Alright, wrong syntax. More learning needed!

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   test "user has email" do
-      user = User.new()
+      user = User.new(:user)
       assert user.valid?
   end
 end
