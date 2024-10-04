d50921eae4baebf3b42459d8066c15f36f9c7691
Imron jehleh <imron.jehleh@gmail.com>
2024-10-04 21:13:24 +0700

    Maybe this was good to be failed? Because we have password constraint.

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
     factory :user do
-        after(:build) { |user| generate_hashed_password(user) }
         email { Faker::Internet.email }
         password { Faker::Internet.password }
     end
